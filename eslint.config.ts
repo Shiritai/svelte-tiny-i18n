bdd42b3bc4625ec923d86537bf48d8e22697ed7c
Tzu-Ching Yang <shingekinocore@gmail.com>
2025-10-26 16:45:40 +0800

    feat: add dev-dep/format/lint/test/build suite

---
+++ eslint.config.ts
@@ -0,0 +1,15 @@
+import prettier from 'eslint-config-prettier';
+import { fileURLToPath } from 'node:url';
+import { includeIgnoreFile } from '@eslint/compat';
+import { defineConfig } from 'eslint/config';
+import ts from 'typescript-eslint';
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+export default defineConfig(includeIgnoreFile(gitignorePath), ...ts.configs.recommended, prettier, {
+    rules: {
+        // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+        // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+        'no-undef': 'off'
+    }
+});
